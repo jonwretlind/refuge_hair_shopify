cbd4ff86024aa15cabe9529d3839a697149c7dc2
jonwretlind <47186515+jonwretlind@users.noreply.github.com>
2022-02-21 16:43:20 -0700

    2-21-2022 updates to megamenu

--- assets/megamenu.js
+++ assets/megamenu.js
@@ -1,27 +1,23 @@
+(function() {
   jQuery(document).ready(function( $ ) {
-    var shopMenu = $('.list-menu li:first-child');
     var megaMenu = $('#Megamenu');
     var wrapper = $('#Megamenu .wrapper');
     var header = $('.header');
-    header.on('scroll', function() {
-      if (! $('div#shopify-section-header').hasClass('shopify-section-header-sticky') ) {
-        header.css({
-          backgroundColor: 'transparent'
-        });
-      };
-    });
-    shopMenu.on("mouseover", function() {
+    var topMenuItem = $('.top-menu li.menu a.menu-link');
+
+    topMenuItem.on("mouseover", function(event) {
       header.css({
       borderBottom: '1px solid rgba(18,18,18,.7)',
       backgroundColor: 'white'
       });
       header.addClass('active');
+      $(event.target).parent().addClass('active');
       megaMenu.css({
         borderBottom: "1px solid rgba(18,18,18,.2)"
       });
       megaMenu.animate({
-        transition: "height .2s ease",
-        height: "300px",
+        transition: "height 1s ease",
+        height: "350px",
         padding: "20px 50px",
       }, .5, function() {
         wrapper.css('display', 'grid');
@@ -31,7 +27,23 @@
       header.addClass('active');
     });
     megaMenu.on("mouseleave", function() {
+      menuOff();
+    });
+    topMenuItem.on('mouseenter', function( event ) {
+      if ( $(event.target).hasClass('has-children') ) {
+        removeActive();
+        var classList = $(event.target).attr("class").split(/\s+/);
+        var num = Number(classList[1].substr(-1, 1));
+        $('.submenu-' + num ).addClass('active');
+      } else  {
+        menuOff();
+      };
+    });
+
+
+    function menuOff() {
       header.removeClass('active');
+      //removeActive();
       header.css({
       borderBottom: '1px solid transparent',
       borderBottom: "unset"
@@ -45,5 +57,11 @@
       }, .5, function() {
         wrapper.css('display', 'none');
       });
-    });
-  });
+    }
+
+    function removeActive() {
+      if ( $('.submenu ul').hasClass('active') ) $('.submenu ul, summary.active').removeClass('active'); // clear active classes
+    }
+
+  }); //document.ready
+})()
